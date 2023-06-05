9582d000a49130ce549398746bbb3add070146ac
Bartosz Iwanicki <bartosz.iwanickii@gmail.com>
2023-06-05 22:29:29 +0200

    wersja 1

---
+++ src/main/java/com/example/demo/SuperStart.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class SuperStart {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
